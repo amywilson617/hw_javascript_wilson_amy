4d37162a5cb66453894b2cc6853bffd7d7ddb726
Amy Wilson <amy.liz@me.com>
2017-05-25 13:14:56 -0700

    adding presidents array, objects, and console.log

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
 var iceCream = ['salted caramel', 'chocolate peanut butter cup', 'birthday cake'];
 
 iceCream.push('cookies and cream');
+
+
+var presidents = [
+
+{ firstName: 'Donald',
+		lastName: 'Trump',
+		termLength: '1 Term',
+		party: 'Republican',
+		yearsOfPresidency: '5 Months',
+	},
+
+{ firstName: 'Barack',
+		lastName: 'Obama',
+		termLength: '2',
+		party: 'Democrat',
+		yearsOfPresidency: '8'
+	},
+
+{ firstName: 'George W.',
+		lastName: ' Bush',
+		termLength: '2',
+		party: 'Republican',
+		yearsOfPresidency: '8'
+	},
+
+{ firstName: 'Bill',
+		lastName: 'Clinton',
+		termLength: '2',
+		party: 'Democrat',
+		yearsOfPresidency: '8'
+	},
+
+{ firstName: 'George H. W.',
+		lastName: 'Bush',
+		termLength: '2',
+		party: 'Republican',
+		yearsOfPresidency: '8'
+	}
+];
+
+console.log(presidents[2].firstName + presidents[2].lastName);
