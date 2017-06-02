ec61be1fee0d01dcaf080c70d4a28c52fb86d7b2
Amy Wilson <amy.liz@me.com>
2017-06-02 07:46:00 -0700

    commiting functions

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 
+
+*The only helpful resource I used was our instructor, Nick Page.
+
+*I did not use any resources outside of the lectures and my notes on this assignment, other than mentioned above.
+
+*I	reviewed	the	list	of	deductions	for	this	project
+and	in	the	syllabus.
+
+*This is the hardest assignment I have done in my time at UF!
+
+*I tried really hard, but feel disappointed in my struggle with this.
--- js/main.js
+++ js/main.js
@@ -2,38 +2,37 @@ var iceCream = ['salted caramel', 'chocolate peanut butter cup', 'birthday cake'
 
 iceCream.push('cookies and cream');
 
-
 var presidents = [
 
-{ firstName: 'Donald',
+	{ firstName: 'Donald',
 		lastName: 'Trump',
 		termLength: '1 Term',
 		party: 'Republican',
 		yearsOfPresidency: '5 Months',
 	},
 
-{ firstName: 'Barack',
+	{ firstName: 'Barack',
 		lastName: 'Obama',
 		termLength: '2',
 		party: 'Democrat',
 		yearsOfPresidency: '8'
 	},
 
-{ firstName: 'George W.',
+	{ firstName: 'George W.',
 		lastName: ' Bush',
 		termLength: '2',
 		party: 'Republican',
 		yearsOfPresidency: '8'
 	},
 
-{ firstName: 'Bill',
+	{ firstName: 'Bill',
 		lastName: 'Clinton',
 		termLength: '2',
 		party: 'Democrat',
 		yearsOfPresidency: '8'
 	},
 
-{ firstName: 'George H. W.',
+	{ firstName: 'George H. W.',
 		lastName: 'Bush',
 		termLength: '2',
 		party: 'Republican',
@@ -42,3 +41,33 @@ var presidents = [
 ];
 
 console.log(presidents[2].firstName + presidents[2].lastName);
+
+function howdy() {
+	console.log('Amy Wilson says hello!');
+}
+howdy();
+
+var numba = function (string) {
+	if (string.length < 7) {
+		console.log('What a short little word!');
+	} else if (string.length > 7) {
+		console.log("I'm sorry, but that's too many to count!");
+	} else if (string.length == 7) {
+		console.log('7, What a perfect choice!');
+	}
+};
+
+numba('7');
+numba('seventy');
+numba('university');
+
+function inception(display, favMovie) {
+	display(favMovie)
+};
+
+
+function movie(myMovie){
+	console.log( myMovie + ' is a great movie.')
+};
+
+inception(movie, 'Harry Potter');
